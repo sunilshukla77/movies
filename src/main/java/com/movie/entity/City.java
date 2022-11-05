69eee1a59398d89d29d18d0e2a65c9c28bbd9223
Sunil Kumar Shukla <sunil@Sunils-MacBook-Pro.local>
2022-11-05 18:21:13 +0530

    fix

--- src/main/java/com/movie/entity/City.java
+++ src/main/java/com/movie/entity/City.java
@@ -1,31 +1,30 @@
 package com.movie.entity;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Accessors(chain = true)
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "CITY")
 public class City {
 
-    private int cityId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CITY_ID")
+    private Long cityId;
+    @Column(name = "CITY_NAME")
     private String cityName;
 
-    public City() {
-    }
-
-    public City(int cityId, String cityName) {
-        this.cityId = cityId;
-        this.cityName = cityName;
-    }
-
-    public int getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(int cityId) {
-        this.cityId = cityId;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
+    /*@ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(name = "city_theatre", joinColumns = @JoinColumn(name = "city_id"),
+            inverseJoinColumns = @JoinColumn(name = "theatre_id"))
+    private Set<Theatre> theatres = new HashSet<>();*/
 }
