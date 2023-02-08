29e10b99ad7f651db984a8afe13dd87430c0f25e
sunil <sunil.jobs77@gmail.com>
2023-02-08 19:42:58 +0530

    Test

--- src/main/java/com/movie/MoviesApplication.java
+++ src/main/java/com/movie/MoviesApplication.java
@@ -1,9 +1,7 @@
 package com.movie;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -12,9 +10,6 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
+
 
 }
---
+++ src/test/java/com/movie/MoviesApplicationTest.java
@@ -0,0 +1,9 @@
+package com.movie;
+
+import org.junit.jupiter.api.Test;
+class MoviesApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+  
+}
